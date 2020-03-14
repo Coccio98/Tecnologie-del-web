9e9d685af3ba59a48a628784931ec3e8a5dd452a
Coccio98 <31818421+Coccio98@users.noreply.github.com>
2020-03-14 17:24:03 +0100

    implemetazione template

---
+++ project/resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+require('./jquery.min');
+
+require('./bootstrap.min');
+
+require('./slick.min');
+
+require('./nouislider.min');
+
+require('./jquery.zoom.min');
+
+require('./main');
+
