55a57bdf05d1c2cce9d48055f7c3a80022802eb8
Jessicaleone <45167493+Jessicaleone@users.noreply.github.com>
2020-03-25 10:15:32 +0100

    account

---
+++ project/resources/views/partials/myaccount/account.blade.php
@@ -0,0 +1,14 @@
+<!-- shop -->
+<a class="cta-btn">
+    <div class="col-md-4 col-xs-6">
+        <div class="shop-account">
+            <div class="shop-img">
+                <img src="{{ $image }}" alt="">
+            </div>
+            <div class="shop-body">
+                <h3>{{$slot}}</h3>
+            </div>
+        </div>
+    </div>
+</a>
+<!-- /shop -->
