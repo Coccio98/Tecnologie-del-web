b5226eb230bce7843f330efe6b9a50517778533a
Coccio98 <31818421+Coccio98@users.noreply.github.com>
2020-03-25 10:55:07 +0100

    test

--- project/resources/resources/views/partials/store/checkbox.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="input-checkbox">
-    <input type="checkbox" id="{{$n}}">
-    <label for="{{$n}}">
-        <span></span>
-        {{$category}}
-        <small>({{$slot}})</small>
-    </label>
-</div>
